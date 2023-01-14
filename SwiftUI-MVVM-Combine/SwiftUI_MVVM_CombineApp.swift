125691f806a417384f5187b60d74476bb418b3fb
mahmoud ismail <mahmoud.ismail2203@gmail.com>
2023-01-14 04:52:55 +0200

    Initial Commit

---
+++ SwiftUI-MVVM-Combine/SwiftUI_MVVM_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_CombineApp.swift
+//  SwiftUI-MVVM-Combine
+//
+//  Created by Mahmoud Ismail on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
